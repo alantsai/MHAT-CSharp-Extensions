7d972a5a04be6c11a73fc17129f45996d779edf9
Alan Tsai <alan@alantsai.net>
2015-09-09 22:36:39 +0800

    add first object extension

---
+++ Src/EssentialExtensionSet/ObjectExtension/ObjectExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EssentialExtensionSet.ObjectExtension
+{
+    /// <summary>
+    /// Extension methods for object
+    /// </summary>
+    public static class ObjectExtension
+    {
+        /// <summary>
+        /// Get string of object; guard against null object with return empty string
+        /// </summary>
+        /// <param name="value">getting string value of object</param>
+        /// <returns>
+        /// If the pass in object is null, return empty string. Else call ToString method on the object.
+        /// </returns>
+        public static string ToStringNullGuard(this object value)
+        {
+            return (value ?? string.Empty).ToString();
+        }
+    }
+}
