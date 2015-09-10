33d6559cd4ecf8d9fa5f04b741b95b70e5570ab0
Alan Tsai <alan@alantsai.net>
2015-09-10 16:11:06 +0800

    add ienumerable as string

---
+++ Src/EssentialExtensionSet/IEnumerableExtension/IEnumerableExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialExtensionSet.IEnumerableExtension
+{
+    /// <summary>
+    /// IEnumerable Extensions
+    /// </summary>
+    public static class IEnumerableExtension
+    {
+        /// <summary>
+        /// Out put enumerable of item as strings
+        /// </summary>
+        /// <typeparam name="T">Any Type</typeparam>
+        /// <param name="value">The value.</param>
+        /// <param name="seperator">The seperator between each value</param>
+        /// <returns>
+        /// String represent of enumrable of items
+        /// </returns>
+        public static string ToConcatString<T>(this IEnumerable<T> value, 
+            string seperator = ",")
+        {
+            return string.Join(seperator, value.Select(x => x.ToString()));
+        }
+    }
+}
