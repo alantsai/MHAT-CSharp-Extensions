e0d677ec5de83b8a7c544978cf86b58813720c4b
Alan Tsai <alan@alantsai.net>
2015-09-10 14:59:22 +0800

    [string]add if string is empty or not

---
+++ Src/EssentialExtensionSet/StringExtension/StringExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialExtensionSet.StringExtension
+{
+    /// <summary>
+    /// String Extensions
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Determines whether value is null, empty or only whitespace
+        /// </summary>
+        /// <param name="value">Test value</param>
+        /// <returns>true if value is null, empty or only whitespace</returns>
+        public static bool IsStringEmpty(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
+        /// Determines whether value is NOT null, empty or only whitespace
+        /// </summary>
+        /// <param name="value">Test value</param>
+        /// <returns>true if value is NOT null, empty or only whitespace</returns>
+        public static bool IsStringNotEmpty(this string value)
+        {
+            return value.IsStringEmpty() == false;
+        }
+    }
+}
